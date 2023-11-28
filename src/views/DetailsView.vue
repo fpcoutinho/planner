2489379ac1f16aa95c799bb9ae0d64713d0a3e43
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-28 10:39:56 -0300

    delete post + edit post mock

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -3,10 +3,21 @@
     <p v-if="error" class="text-error">{{ error }}</p>
     <div v-if="post" class="post prose max-w-none">
       <h2>{{ post.title }}</h2>
-      <div class="flex flex-row gap-1 justify-end">
-        <router-link v-for="tag in post.tags" :key="tag" class="no-underline" :to="{ name: 'Tag', params: { tag: tag } }">
-          <span class="badge badge-primary badge-outline">#{{ tag }}</span>
-        </router-link>
+      <div class="submenu flex flex-row justify-between">
+        <div class="flex flex-row gap-1">
+          <router-link v-for="tag in post.tags" :key="tag" class="no-underline"
+            :to="{ name: 'Tag', params: { tag: tag } }">
+            <span class="badge badge-primary badge-outline">#{{ tag }}</span>
+          </router-link>
+        </div>
+        <div class="buttons flex flex-row gap-1">
+          <button class="btn btn-primary btn-outline btn-sm" @click="editPost">
+            Edit Post
+          </button>
+          <button class="btn btn-error btn-outline btn-sm" @click="deletePost">
+            Delete Post
+          </button>
+        </div>
       </div>
       <p>{{ post.body }}</p>
     </div>
@@ -16,7 +27,11 @@
 
 <script setup>
 import { onMounted } from 'vue';
+import { projectFirestore } from '../firebase/config';
+import { useRouter } from 'vue-router';
 import getPost from '../composables/getPost';
+
+const router = useRouter()
 
 const props = defineProps({
   id: {
@@ -30,4 +45,16 @@ const { post, error, load } = getPost(props.id)
 onMounted(() => {
   load()
 })
+
+const editPost = () => {
+  console.log('edit post')
+}
+
+const deletePost = async () => {
+  if (confirm('Are you sure you want to delete this post?')) {
+    await projectFirestore.collection('posts').doc(props.id).delete()
+
+    router.push({ name: 'Home' })
+  }
+}
 </script>
