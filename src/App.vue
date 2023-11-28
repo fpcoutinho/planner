2a47788aae1fd9b3aae4592fab6d25fa7bf0a6d9
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-28 10:25:37 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
   <main class="flex flex-col w-full mt-16 px-5 md:px-20">
     <router-view />
   </main>
+  <FooterBar />
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue';
+import NavBar from './components/NavBar.vue'
+import FooterBar from './components/FooterBar.vue'
 </script>
 
 <style>
@@ -14,6 +16,8 @@ import NavBar from './components/NavBar.vue';
   font-family: 'Source Sans 3 Variable', sans-serif;
   min-width: 100vw;
   min-height: 100vh;
+  position: relative;
+  padding-bottom: 220px;
   display: flex;
   flex-direction: column;
   align-items: center;
